f654e7e2334addd074c2116414595f4aafdc6bc1
NMiroshnichenko <mironike@yandex.ru>
2017-05-22 18:58:06 +0300

    StringHandler add sizeOfReductedLine method

--- one/src/StringHandler.java
+++ one/src/StringHandler.java
@@ -1,15 +1,29 @@
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.IntStream;
 
-/**
- * Created by NMiroshnichenko on 22.05.2017.
- */
+
 public class StringHandler {
-    private Stream<Integer> answer;
+    private List<Long> answer;
+
 
     public void handleString(String newLine) {
+        answer.add(sizeOfReductedLine(newLine));
     }
 
-    public Stream<Integer> getAnswer() {
+    private long sizeOfReductedLine(String newLine) {
+        IntStream lineAsStream = newLine.chars();
+        long result = lineAsStream.count();
+
+        long countA = lineAsStream.filter(el -> el == 'a').count();
+        long countB = lineAsStream.filter(el -> el == 'b').count();
+        long countC = lineAsStream.filter(el -> el == 'c').count();
+
+        if ((countA == 0 && countB == 0) ^ (countA == 0 && countC == 0) ^ (countC == 0 && countB == 0)) return result;
+        if ((countA % 2 == 0) & (countB % 2 == 0) & (countC % 2 == 0)) return 2;
+        else return 1;
+    }
+
+    public List<Long> getAnswer() {
         return answer;
     }
 
