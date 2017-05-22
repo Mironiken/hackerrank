47d16db94d5d4dea2e1ba7e92e0881dbfed570aa
NMiroshnichenko <mironike@yandex.ru>
2017-05-22 17:54:48 +0300

    initial commit Reduction project

---
+++ one/src/Solution.java
@@ -0,0 +1,8 @@
+/**
+ * Created by NMiroshnichenko on 22.05.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ one/src/StringHandler.java
@@ -0,0 +1,16 @@
+import java.util.stream.Stream;
+
+/**
+ * Created by NMiroshnichenko on 22.05.2017.
+ */
+public class StringHandler {
+    private Stream<Integer> answer;
+
+    public void handleString(String newLine) {
+    }
+
+    public Stream<Integer> getAnswer() {
+        return answer;
+    }
+
+}
