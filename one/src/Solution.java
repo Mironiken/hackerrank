d72ee2092c54cf49bc28b2a73d8ef77df885d25a
NMiroshnichenko <mironike@yandex.ru>
2017-05-22 19:04:43 +0300

    add psvm part

--- one/src/Solution.java
+++ one/src/Solution.java
@@ -1,8 +1,18 @@
-/**
- * Created by NMiroshnichenko on 22.05.2017.
- */
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringHandler solver = new StringHandler();
+
+        int numberOfLines = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < numberOfLines; i++) {
+            solver.handleString(sc.nextLine());
+        }
+
+        solver.getAnswer().stream().forEach(el -> System.out.println(el));
 
     }
 }
